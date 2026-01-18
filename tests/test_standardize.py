0c7ac453ee553002ff39bd43f4a0e42208a82593
stefanpate <stefanpate94@gmail.com>
2026-01-18 13:56:10 -0600

    fixed typo in test suite from function name change

--- tests/test_standardize.py
+++ tests/test_standardize.py
@@ -1,4 +1,4 @@
-from ergochemics.standardize import standardize_smiles, standardize_reaction, hash_reaction, hash_compound
+from ergochemics.standardize import standardize_smiles, standardize_reaction, hash_reaction, hash_molecule
 
 smi = 'O=C(O)CC[c-]1[nH]cnc1=O'
 rxn = 'O=C(O)CC[c-]1[nH]cnc1=O.O=C(O)CC[c-]1[nH]cnc1=O>>O=C(O)CC[c-]1[nH]cnc1=O.O=C(O)CC[c-]1[nH]cnc1=O'
@@ -7,4 +7,4 @@ def test_standardize_reaction():
     assert hash_reaction(standardize_reaction(rxn)) == hash_reaction(standardize_reaction(rxn))
 
 def test_standardize_smiles():
-    assert hash_compound(standardize_smiles(smi)) == hash_compound(standardize_smiles(smi))
+    assert hash_molecule(standardize_smiles(smi)) == hash_molecule(standardize_smiles(smi))
